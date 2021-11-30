dd2d2f3e211ffec3864098387c161197a4759206
EC2 Default User <ec2-user@ip-172-31-29-34.ap-northeast-1.compute.internal>
2021-11-30 04:46:47 +0000

    stopwatch_1130

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
-$(document).ready(function(){
-  let sec =0;
-  let min =0;
-  let timer;
+/*global $*/
+$(function() {
+  var fps =0;
+  var sec =0;
+  var min =0;
+  var hour =0;
+  var timer;
  
  //スタート 
 $(".start").click(function() {
   sec = 0;
   min = 0;
+  hour = 0;
 
   $(".clock").html("0:0:0:0");
   timer = setInterval(countup, 100);
@@ -26,6 +30,7 @@ $(".stop").click(function(){
 $(".reset").click(function() {
   sec = 0;
   min = 0;
+  hour =0;
 
   $(".clock").html("0:0:0:0");
   clearInterval(timer);
@@ -35,14 +40,31 @@ $(".reset").click(function() {
 });
 
 // カウントアップ
-  function countup()
+ function countup()
   {
-    sec += 1;
+    fps += 1;
+
+    if (fps > 9) {
+      fps = 0;
+      sec += 1;
+    }
 
     if (sec > 59) {
       sec = 0;
       min += 1;
     }
+    
+    if (min > 59) {
+      min = 0;
+      hour += 1;
     }
+    
+ // 0埋め
+  //fps_number = ("0" + fps).slice(-2);
+  //sec_number = ("0" + sec).slice(-2);
+  //min_number = ("0" + min).slice(-2);
+  //hour_number = ("0" + hour).slice(-2);
 
-})
+  $(".clock").html(hour + ':' + min + ':' +  sec + ':' + fps);
+  }
+});
